20d8d79ece6144a0982938608c7718e7b9700dfb
Artem Deviatov <dayddhost@yandex.ru>
2026-07-05 20:29:25 +0500

    Step 13: run_market_making_episode

--- model.py
+++ model.py
@@ -154,8 +154,48 @@ def update_fair_value_from_trade(fair_value, side, bid, ask, adjustment):
 # Step 12 - update_remaining_card_value (not yet solved)
 # TODO: implement
 
-# Step 13 - run_market_making_episode (not yet solved)
-# TODO: implement
+# Step 13 - run_market_making_episode
+def run_market_making_episode(true_value, counterparty_sides, initial_fair_value, config):
+    base_spread = config.get('base_spread', 0.0)
+    uncertainty = config.get('uncertainty', 0.0)
+    skew_strength = config.get('skew_strength', 0.0)
+    belief_adjustment = config.get('belief_adjustment', 0.0)
+    
+    cash = 0.0
+    inventory = 0
+    fair_value = float(initial_fair_value)
+    history = []
+    
+    for side in counterparty_sides:
+        spread_width = uncertainty_spread(base_spread, uncertainty)
+        quotes = inventory_skewed_quotes(fair_value, spread_width, inventory, skew_strength)
+        bid = quotes['bid']
+        ask = quotes['ask']
+        
+        current_state = {'cash': cash, 'inventory': inventory}
+        new_state = execute_trade(current_state, side, bid, ask, size=1)
+        cash = new_state['cash']
+        inventory = new_state['inventory']
+        fair_value = update_fair_value_from_trade(fair_value, side, bid, ask, belief_adjustment)
+        
+        history.append({
+            'bid': bid,
+            'ask': ask,
+            'side': side,
+            'cash': cash,
+            'inventory': inventory,
+            'fair_value': fair_value
+        })
+        
+    final_pnl = mark_to_market_pnl(cash, inventory, true_value)
+    
+    return {
+        'pnl': final_pnl,
+        'cash': cash,
+        'inventory': inventory,
+        'fair_value': fair_value,
+        'history': history
+    }
 
 # Step 14 - summarize_episode_pnls (not yet solved)
 # TODO: implement
